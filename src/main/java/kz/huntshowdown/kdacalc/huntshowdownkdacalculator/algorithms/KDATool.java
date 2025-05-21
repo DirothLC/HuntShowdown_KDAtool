8bf3c9dbc1e3ea82e79387dbbc56cb93aa333f27
Kutsinas Darius <dkuc222@gmail.com>
2025-05-21 15:10:19 +0500

    Added deathsToLowerKDA method if desiredKDA < current KDA

--- src/main/java/kz/huntshowdown/kdacalc/huntshowdownkdacalculator/algorithms/KDATool.java
+++ src/main/java/kz/huntshowdown/kdacalc/huntshowdownkdacalculator/algorithms/KDATool.java
@@ -15,8 +15,24 @@ public class KDATool {
         return kills - deaths;
     }
 
-    public int desiredKDA(int kills, int assists, int deaths,  double desiredKDA){
-        double missingKills = desiredKDA * deaths - kills - assists;
-        return (int) Math.ceil(Math.max(0, missingKills));
+    public int desiredKDA(int kills, int assists, int deaths,  double desiredKDA) {
+        double kda = (double) (kills + assists) / deaths;
+        double missingKills=0;
+        if (desiredKDA < kda) {
+           missingKills =   deathsToLowerKDA(kills, assists, deaths, desiredKDA);
+
+        } else {
+            missingKills = desiredKDA * deaths - kills - assists;
+        }
+            return (int) Math.ceil(Math.max(0, missingKills));
+
     }
+
+
+    public int deathsToLowerKDA(int kills, int assists,int deaths, double desiredKDA){
+        double requiredDeaths = (kills + assists) / desiredKDA;
+        double result= requiredDeaths - deaths;
+        return (int) Math.ceil(Math.max(0, result));
+    }
+
 }
